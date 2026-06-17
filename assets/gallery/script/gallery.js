fff489bd5839ea63950c80ef82f848b0f62f718d
SSF KARIPARAMBA UNIT <swalihkariparamba@gmail.com>
2026-06-17 14:47:00 +0530

    Update gallery.js

--- assets/gallery/script/gallery.js
+++ assets/gallery/script/gallery.js
@@ -1,1 +1,61 @@
+//NAV SCROLL
 
+const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", () => {
+if(window.scrollY > 50){
+navbar.classList.add("scrolled");
+}else{
+navbar.classList.remove("scrolled");
+}
+});
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+const menuIcon = menuBtn.querySelector("i");
+menuBtn.addEventListener("click", () => {
+navLinks.classList.toggle("active");
+if(navLinks.classList.contains("active")){
+menuIcon.classList.remove("fa-bars");
+menuIcon.classList.add("fa-xmark");
+}else{
+menuIcon.classList.remove("fa-xmark");
+menuIcon.classList.add("fa-bars");
+}
+});
+
+//IMAGES SCROLL
+
+document.querySelectorAll('.Im').forEach(function(img){
+let images = img.getAttribute('data-images').split(',');
+let current = 0;
+setInterval(function(){
+current++;
+if(current >= images.length){
+current = 0;
+}
+img.src = images[current].trim();
+}, 3000);
+});
+
+//ON CLICK
+
+function go(url){
+window.location.href = url;
+}
+//LONG PRESS BLOCK
+document.addEventListener("contextmenu", function(e){
+e.preventDefault();
+});
+
+//NAVBAR WORK
+
+document.addEventListener("click", function(e){
+if(
+navLinks.classList.contains("active") &&
+!navLinks.contains(e.target) &&
+!menuBtn.contains(e.target)
+){
+navLinks.classList.remove("active");
+menuIcon.classList.remove("fa-xmark");
+menuIcon.classList.add("fa-bars");
+}
+});
